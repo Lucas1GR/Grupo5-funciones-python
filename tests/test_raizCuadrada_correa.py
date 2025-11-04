9905ff9713729d6448685c0ad82676e7ca9ff5de
HoracioIsmael <horacioismaelcorrea@gmail.com>
2025-11-04 19:46:30 -0300

    Agrega funcion correa

---
+++ tests/test_raizCuadrada_correa.py
@@ -0,0 +1,7 @@
+#tests/test_raiz_cuadrada.py
+
+from funciones.raiz_cuadrada import raiz_cuadrada
+
+def test_raiz_cuadrada():
+    assert raiz_cuadrada(9) == 3
+    assert raiz_cuadrada(-4) is None
